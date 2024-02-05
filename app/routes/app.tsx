17d69568d9eb00db990876eabaf2cdc47666a0cc
BrunoPanizzi <brunopanizziq@gmail.com>
2024-02-04 21:33:36 -0300

    added graph

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useState } from 'react'
+import * as d3 from 'd3'
+import { ReactNode, useEffect, useState } from 'react'
 import {
   ActionFunctionArgs,
   LoaderFunctionArgs,
@@ -140,7 +141,7 @@ export default function App() {
 
       <Wrapper>
         <div className="grid gap-4 md:grid-cols-2 ">
-          <div className="@container/list flex flex-col gap-2">
+          <div className="flex flex-col gap-2 @container/list">
             <ListHeader />
 
             {wallets.map((w) => (
@@ -148,23 +149,70 @@ export default function App() {
             ))}
           </div>
 
-          <div className="min-h-60 bg-emerald-400/75" />
-
-          {/* <Collapsible orientation="vertical">
-                    <div
-                      ref={setContainerRef}
-                      class=" h-96 rounded-r-xl bg-white/5 text-emerald-500"
-                    >
-                      <Graph
-                        data={wallets}
-                        w={size.width || 0}
-                        h={size.height || 0}
-                        m={16}
-                      />
-                    </div>
-                  </Collapsible> */}
+          <div>
+            <h2 className="mb-2 flex-1 text-2xl font-semibold text-emerald-50">
+              Distribuição dos ativos:
+            </h2>
+            <div className="h-min rounded-xl bg-gray-700/50">
+              <div className="mx-auto max-w-md">
+                <Graph data={wallets} m={5} />
+              </div>
+            </div>
+          </div>
         </div>
       </Wrapper>
+    </>
+  )
+}
+
+type GraphProps = {
+  data: W[]
+  w?: number
+  h?: number
+  m?: number
+}
+
+function Graph({ data, h = 100, w = 100, m = 0 }: GraphProps) {
+  const treatedData = data.filter((d) => d.totalValue > 0)
+  const r = Math.min(w, h) / 2 - m
+
+  let pie = d3
+    .pie<W>()
+    .value((d) => d.totalValue)
+    .padAngle(0.03)
+
+  let angles = pie(treatedData)
+
+  let arcGenerator = d3
+    .arc<d3.PieArcDatum<W>>()
+    .innerRadius(r / 2)
+    .outerRadius(r)
+
+  const arcs = angles.map((d) => ({
+    d: arcGenerator(d)!,
+    title: d.data.title,
+    color: d.data.color,
+  }))
+
+  return (
+    <>
+      <svg viewBox={`0 0 ${w} ${h}`}>
+        {/* <TotalValue
+          totalValue={hoveredWallet()?.totalValue || totalValue()}
+          color={hoveredWallet()?.color || 'emerald'}
+          radius={r()}
+        /> */}
+        <g transform={`translate(${w / 2}, ${h / 2})`}>
+          {arcs.map((d) => (
+            <path
+              key={d.d}
+              d={d.d}
+              data-color={d.color}
+              className="fill-primary-600 transition-transform hover:scale-105"
+            />
+          ))}
+        </g>
+      </svg>
     </>
   )
 }
@@ -236,7 +284,7 @@ function Wallet({ wallet }: { wallet: W }) {
 function ListHeader() {
   return (
     <>
-      <span className="@md/list:mb-0 mb-2 flex items-center gap-4">
+      <span className="mb-2 flex items-center gap-4 @md/list:mb-0">
         <h2 className="flex-1 text-2xl font-semibold text-emerald-50">
           Suas carteiras:
         </h2>
@@ -251,7 +299,7 @@ function ListHeader() {
           </PopoverContent>
         </Popover>
       </span>
-      <menu className="@md/list:flex mb-3 hidden gap-1 rounded-xl bg-gray-700 p-1">
+      <menu className="mb-3 hidden gap-1 rounded-xl bg-gray-700 p-1 @md/list:flex">
         <ToolBarContent />
       </menu>
     </>

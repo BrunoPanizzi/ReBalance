e4ef9c9dc9fd6a06b26bacd638a82eaf700dedb2
BrunoPanizzi <brunopanizziq@gmail.com>
2024-02-06 16:38:32 -0300

    added walletWithStocks and walletWithStocksAndPrices methods

--- app/services/walletService/index.ts
+++ app/services/walletService/index.ts
@@ -6,6 +6,19 @@ import type {
   Wallet,
   WalletWithStocks,
 } from '~/services/db/schema/wallet.server'
+import { Stock } from '../db/schema/stock.server'
+
+import MarketService from '~/services/maketService/index.server'
+
+export type StockWithPrice = Stock & {
+  price: number
+  totalValue: number
+  percentage: number
+}
+
+export type WalletWithStocksAndPrices = Wallet & {
+  stocks: StockWithPrice[]
+}
 
 // TODO: return Result type for better error handling
 class WalletService {
@@ -17,26 +30,75 @@ class WalletService {
     return wallets
   }
 
-  getWallet(
-    uid: string,
-    id: string,
-    withStocks: true,
-  ): Promise<WalletWithStocks | undefined>
-  getWallet(
-    uid: string,
-    id: string,
-    withStocks?: false,
-  ): Promise<Wallet | undefined>
-  async getWallet(uid: string, id: string, withStocks: boolean = false) {
+  async getWallet(uid: string, id: string): Promise<Wallet | undefined> {
     const wallet = await db.query.wallet.findFirst({
       where: (wallet, { and, eq }) =>
         and(eq(wallet.owner, uid), eq(wallet.id, id)),
-      with: {
-        stocks: withStocks ? true : undefined,
-      },
     })
 
     return wallet
+  }
+
+  async getWalletWithStocks(
+    uid: string,
+    id: string,
+  ): Promise<WalletWithStocks | undefined> {
+    const wallet = await db.query.wallet.findFirst({
+      where: (wallet, { eq, and }) =>
+        and(eq(wallet.owner, uid), eq(wallet.id, id)),
+      with: { stocks: true },
+    })
+
+    return wallet
+  }
+
+  async getWalletWithStocksAndPrices(
+    uid: string,
+    id: string,
+  ): Promise<WalletWithStocksAndPrices | undefined> {
+    const wallet = await this.getWalletWithStocks(uid, id)
+
+    if (!wallet) return undefined
+
+    const marketData = await MarketService.getManyStocksData(
+      wallet.stocks.map((stock) => stock.ticker),
+    )
+
+    const stocksWithPrices = wallet.stocks.map((stock) => {
+      const stockData = marketData.find((data) => data.symbol === stock.ticker)
+
+      if (!stockData) {
+        // should not happen, but just to make typescript happy
+        throw new Error(
+          `Stock data for ${stock.ticker} is not present in marketData.`,
+        )
+      }
+
+      const price = stockData.regularMarketPrice
+      const totalValue = stock.amount * price
+
+      return {
+        ...stock,
+        price,
+        totalValue,
+      }
+    })
+
+    const totalValue = stocksWithPrices.reduce(
+      (acc, stock) => acc + stock.totalValue,
+      0,
+    )
+
+    const stocks: StockWithPrice[] = stocksWithPrices.map((stock) => ({
+      ...stock,
+      percentage: stock.totalValue / totalValue,
+    }))
+
+    return {
+      ...wallet,
+      totalValue,
+      stocks,
+    }
   }
 
   async createWallet(uid: string, wallet: NewWallet): Promise<Wallet> {

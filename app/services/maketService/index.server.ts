67ce986359d09cde046e00529b68aaedf308ac75
BrunoPanizzi <brunopanizziq@gmail.com>
2024-02-06 16:38:13 -0300

    added marketservice

---
+++ app/services/maketService/index.server.ts
@@ -0,0 +1,64 @@
+import { z } from 'zod'
+
+const TOKEN = process.env.BRAPI_TOKEN
+
+if (!TOKEN) {
+  throw new Error('BRAPI_TOKEN should be defined in .env file.')
+}
+
+const BASE_URL = 'https://brapi.dev/api'
+
+const responseSchema = z.object({
+  results: z.array(
+    z.object({
+      currency: z.string(),
+      shortName: z.string(),
+      longName: z.string(),
+      symbol: z.string(),
+      logourl: z.string(),
+      regularMarketPrice: z.number(),
+    }),
+  ),
+  requestedAt: z.string(),
+})
+
+const stockDataSchema = z.object({
+  currency: z.string(),
+  shortName: z.string(),
+  longName: z.string(),
+  symbol: z.string(),
+  logourl: z.string(),
+  regularMarketPrice: z.number(),
+})
+
+type StockData = z.infer<typeof stockDataSchema>
+
+class MarketService {
+  async getStockData(symbol: string): Promise<StockData> {
+    const response = await fetch(`${BASE_URL}/quote/${symbol}?token=${TOKEN}`)
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch stock data. Error: ' + response.status)
+    }
+
+    const parsedResponse = responseSchema.parse(await response.json())
+
+    const result = parsedResponse.results[0]
+
+    if (result.symbol !== symbol) {
+      throw new Error('Failed to fetch stock data. Symbol mismatch.')
+    }
+
+    return result
+  }
+
+  async getManyStocksData(symbols: string[]): Promise<StockData[]> {
+    const promises = symbols.map((symbol) => this.getStockData(symbol))
+
+    const responses = await Promise.all(promises)
+
+    return responses
+  }
+}
+
+export default new MarketService()

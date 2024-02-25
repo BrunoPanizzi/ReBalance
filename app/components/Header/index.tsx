f5b403e4190ee3d25a60514c2901128e4679918d
Bruno Panizzi <brunopanizziq@gmail.com>
2024-02-24 23:42:59 -0300

    made back arrow more responsive

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -21,8 +21,8 @@ export default function Header({
     <header className="mx-auto mb-8 flex items-center justify-between gap-10 border-0 border-b border-primary-300/10 bg-gray-700 p-3 shadow-xl shadow-primary-950/10 backdrop-blur sm:mt-4 sm:w-[min(80rem,calc(100%-2rem))] sm:rounded-xl sm:border sm:p-4">
       <div className="flex items-center justify-center gap-3">
         {backArrow && (
-          <button onClick={() => navigate(-1)} title="Voltar">
-            <ArrowLeftIcon className="h-8 w-8 text-primary-100" />
+          <button onClick={() => navigate('/app')} title="Voltar">
+            <ArrowLeftIcon className="size-6 text-primary-100 sm:size-8" />
           </button>
         )}
 

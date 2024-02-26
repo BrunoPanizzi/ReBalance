4c196d49e201ae7fba9b33e379b9b43a6ab48d8a
Bruno Panizzi <brunopanizziq@gmail.com>
2024-02-26 15:43:22 -0300

    added textarea component

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -25,4 +25,25 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 )
 Input.displayName = 'Input'
 
-export { Input }
+export interface TextAreaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          'flex max-h-56 min-h-32 resize-y rounded-md border border-gray-800 bg-gray-500/25 px-3 py-2 text-sm shadow transition-colors placeholder:text-gray-400 sm:text-base',
+          'hover:border-primary-500/25 focus-visible:border-primary-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50',
+          'data-[error=true]:border-red-400/75 hover:data-[error=true]:border-red-400',
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
+TextArea.displayName = 'TextArea'
+
+export { Input, TextArea }

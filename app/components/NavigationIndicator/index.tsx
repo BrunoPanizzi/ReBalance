fdfb2fdad2929f2ff132f2d670a87ba9afdd9cee
Bruno Panizzi <brunopanizziq@gmail.com>
2025-01-22 21:57:37 -0300

    improve loader animation
    c

--- app/components/NavigationIndicator/index.tsx
+++ app/components/NavigationIndicator/index.tsx
@@ -1,25 +1,41 @@
 import { useNavigation } from '@remix-run/react'
-import { AnimatePresence, motion } from 'framer-motion'
+import {
+  AnimatePresence,
+  motion,
+  useMotionTemplate,
+  useTime,
+  useTransform,
+} from 'framer-motion'
 
 export default function NavigationIndicator() {
   const { state } = useNavigation()
+  const shouldShowLoader = state === 'loading'
 
   return (
     <AnimatePresence>
-      {state === 'loading' && (
-        <motion.div
-          className="fixed left-0 top-0 z-50 h-1 bg-primary-500"
-          animate={{
-            width: '100%',
-            transition: {
-              type: 'tween',
-              easings: [0.1, 1, 0.1, 1],
-              duration: 5,
-            },
-          }}
-          exit={{ width: '100%', x: '100%', transition: { duration: 0.5 } }}
-        />
-      )}
+      {shouldShowLoader && <Bar key={Date.now()} />}
     </AnimatePresence>
+  )
+}
+
+function Bar() {
+  const time = useTime()
+
+  const progress = useTransform(() => (1 - 1 / (time.get() / 200 + 1)) * 100)
+  const width = useMotionTemplate`${progress}%`
+
+  return (
+    <motion.div
+      className="fixed left-0 top-0 z-50 bg-primary-500"
+      initial={{ width: '0%', opacity: 0, height: 0 }}
+      style={{ width }}
+      animate={{ opacity: 1, height: '4px' }}
+      exit={{
+        x: '100%',
+        opacity: 0,
+        height: 0,
+        transition: { duration: 0.5 },
+      }}
+    />
   )
 }
